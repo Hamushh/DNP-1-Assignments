33031c149c9537361c2ca05e0383be88f048a3c8
Hamushh <sheikhdon389@gmail.com>
2025-09-11 11:26:50 +0200

    Assignment-1

--- Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59309825b86f42eaa2815c594f4259ea9a69721b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf32b47d6a05acd11f2348849bf7950cea4a187c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
